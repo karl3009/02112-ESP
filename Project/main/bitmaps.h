e1b5b5975ef029f787eaac50ad6d92f42909834b
vicclu <ding.victor@gmail.com>
2024-01-18 14:56:51 +0100

    s

--- Project/main/bitmaps.h
+++ Project/main/bitmaps.h
@@ -2,6 +2,11 @@
 #define BITMAPS_H
 #include <stdint.h>
 
-const uint8_t *happyFace();
+const uint8_t *Sad_face();
+const uint8_t *Sad_med_face();
+const uint8_t *Med_face();
+const uint8_t *Hap_med_face();
+const uint8_t *Hap_face();
+
 
 #endif
